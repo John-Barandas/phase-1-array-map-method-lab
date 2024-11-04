32d12961a9b82275bcbefdcc7b611a45081e18bb
John <jwbarand@gmail.com>
2024-11-03 16:36:47 -0800

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    let tutorialTitleCased = "";
+    let getCapitalizedFoo = true;
+    for (let capped = 0; capped < tutorial.length; capped++) {
+      if (getCapitalizedFoo && tutorial[capped] !== " ") {
+        tutorialTitleCased += tutorial[capped].toUpperCase();
+        getCapitalizedFoo = false;
+      } else {
+        tutorialTitleCased += tutorial[capped];
+      }
+
+      if (tutorial[capped] === " ") {
+        getCapitalizedFoo = true;
+      }
+    }
+    return tutorialTitleCased;
+  });
 }
